6fd2f81e1f00ddf55bae671945140d8e0e3c63b9
McEnaney <mcenaneys@icloud.com>
2025-05-29 17:25:23 +0400

    Update settings.py
    New AWS EC2 IP address instance. This is valid only for this instance and thereafter should be removed and replaced with another is EC2 is being used.

--- patientSystem/demo/demo/settings.py
+++ patientSystem/demo/demo/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-q0fml5j-c1bec7ct1s8c42#=81=9t1n_(jm6dajqly#)zg^9ys
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['13.218.45.54']
+ALLOWED_HOSTS = ['3.85.128.182']
 
 
 # Application definition
